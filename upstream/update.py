13e534e828b4dfc123d11db82f9d9abea11b0598
Adam Dyess <adam.dyess@canonical.com>
2022-06-08 20:06:35 -0500

    Update update.py

--- upstream/update.py
+++ upstream/update.py
@@ -188,7 +188,7 @@ def images(component: Release) -> Generator[str, None, None]:
     """Yield all images from each release."""
     with Path(component.path).open() as fp:
         for line in fp:
-            m = IMG_RE.match(line):
+            m = IMG_RE.match(line)
             if m:
                 yield m.groups()[0]
 
