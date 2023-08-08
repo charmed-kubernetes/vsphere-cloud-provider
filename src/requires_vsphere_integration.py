aff5ce85b0d91a82342c636a83a9aafae25a0ea6
Adam Dyess <adam.dyess@canonical.com>
2023-08-08 15:17:17 -0500

    avoid jsonschema and switch to pydantic==1.* to avoid rust libraries (#30)
    * avoid jsonschema and switch to pydantic==1.* to avoid rust libraries
    
    * lint adjustments
    
    * Used the values from the pydantic object
    
    * handle integration relation in such a way to parse JSON strings from each data value
    
    * test parsing of vsphere integration relation data
    
    * fix linting issue

--- src/requires_vsphere_integration.py
+++ src/requires_vsphere_integration.py
@@ -5,40 +5,33 @@
 This only implements the requires side, currently, since the integrator
 is still using the Reactive Charm framework self.
 """
-import json
 import logging
 from typing import Optional
 
-import jsonschema
 from backports.cached_property import cached_property
 from ops.charm import RelationBrokenEvent
 from ops.framework import Object
+from pydantic import BaseModel, Extra, Json, ValidationError
 
 log = logging.getLogger(__name__)
+
+
+class VsphereIntegrationData(BaseModel, extra=Extra.allow):
+    """Requires side of schema of vsphere-integration relation."""
+
+    datacenter: Json[str]
+    datastore: Optional[Json[str]]
+    folder: Optional[Json[str]]
+    password: Json[str]
+    respool_path: Optional[Json[str]]
+    user: Json[str]
+    vsphere_ip: Json[str]
 
 
 class VsphereIntegrationRequires(Object):
     """Requires side of vsphere-integration relation."""
 
     LIMIT = 1
-    SCHEMA = dict(
-        type="object",
-        properties={
-            "datacenter": dict(type="string"),
-            "datastore": dict(type="string"),
-            "folder": dict(type="string"),
-            "password": dict(type="string"),
-            "respool_path": dict(type="string"),
-            "user": dict(type="string"),
-            "vsphere_ip": dict(type="string"),
-        },
-        required=["datacenter", "vsphere_ip", "user", "password"],
-    )
-    IGNORE_FIELDS = {
-        "egress-subnets",
-        "ingress-address",
-        "private-address",
-    }
 
     def __init__(self, charm, endpoint="vsphere-integration"):
         super().__init__(charm, f"relation-{endpoint}")
@@ -51,19 +44,15 @@ class VsphereIntegrationRequires(Object):
         return self.model.get_relation(self.endpoint)
 
     @cached_property
-    def _data(self):
-        if not (self.relation and self.relation.units):
-            return {}
-        raw_data = self.relation.data[list(self.relation.units)[0]]
-        data = {}
-        for field, raw_value in raw_data.items():
-            if field in self.IGNORE_FIELDS or not raw_value:
-                continue
-            try:
-                data[field] = json.loads(raw_value)
-            except json.JSONDecodeError as e:
-                log.error(f"Failed to decode relation data in {field}: {e}")
-        return data
+    def _raw_data(self):
+        if self.relation and self.relation.units:
+            return self.relation.data[list(self.relation.units)[0]]
+        return None
+
+    @cached_property
+    def _data(self) -> Optional[VsphereIntegrationData]:
+        raw = self._raw_data
+        return VsphereIntegrationData(**raw) if raw else None
 
     def evaluate_relation(self, event) -> Optional[str]:
         """Determine if relation is ready."""
@@ -77,51 +66,63 @@ class VsphereIntegrationRequires(Object):
         return None
 
     @property
-    def is_ready(self):
+    def is_ready(self) -> bool:
         """Whether the request for this instance has been completed."""
         try:
-            jsonschema.validate(self._data, self.SCHEMA)
-        except jsonschema.ValidationError:
-            log.error(f"{self.endpoint} relation data not yet valid.")
+            self._data
+        except ValidationError as ve:
+            log.error(f"{self.endpoint} relation data not yet valid. ({ve}")
+            return False
+        if self._data is None:
+            log.error(f"{self.endpoint} relation data not yet available.")
             return False
         return True
 
-    def _value(self, key):
-        if not self._data:
-            return None
-        return self._data.get(key)
-
     @property
-    def datacenter(self):
+    def datacenter(self) -> Optional[str]:
         """The datacenter value."""
-        return self._value("datacenter")
+        if not self.is_ready:
+            return None
+        return self._data.datacenter
 
     @property
-    def datastore(self):
+    def datastore(self) -> Optional[str]:
         """The datastore value."""
-        return self._value("datastore")
+        if not self.is_ready:
+            return None
+        return self._data.datastore
 
     @property
-    def folder(self):
+    def folder(self) -> Optional[str]:
         """The folder value."""
-        return self._value("folder")
+        if not self.is_ready:
+            return None
+        return self._data.folder
 
     @property
-    def user(self):
+    def user(self) -> Optional[str]:
         """The user value."""
-        return self._value("user")
+        if not self.is_ready:
+            return None
+        return self._data.user
 
     @property
-    def password(self):
+    def password(self) -> Optional[str]:
         """The password value."""
-        return self._value("password")
+        if not self.is_ready:
+            return None
+        return self._data.password
 
     @property
-    def respool_path(self):
+    def respool_path(self) -> Optional[str]:
         """The respool_path value."""
-        return self._value("respool_path")
+        if not self.is_ready:
+            return None
+        return self._data.respool_path
 
     @property
-    def vsphere_ip(self):
+    def vsphere_ip(self) -> Optional[str]:
         """The vsphere_ip value."""
-        return self._value("vsphere_ip")
+        if not self.is_ready:
+            return None
+        return self._data.vsphere_ip
---
+++ tests/unit/test_vsphere_integration.py
@@ -0,0 +1,16 @@
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+#
+# Learn more about testing at: https://juju.is/docs/sdk/testing
+
+from pathlib import Path
+
+import yaml
+
+from requires_vsphere_integration import VsphereIntegrationData
+
+
+def test_parse_relation_data():
+    d = yaml.safe_load(Path("tests/data/vsphere_integration_data.yaml").read_text())
+    loaded = VsphereIntegrationData(**d)
+    assert loaded.password == "<super-secret>"
